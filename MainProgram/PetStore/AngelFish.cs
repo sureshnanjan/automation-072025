22b66ed06a4a5b18de21b21021fb3dcb8b3ec087
sureshnanjan <sureshnanjan@outlook.com>
2025-07-17 11:50:58 +0530

    Demo for Interfaces Has custom interface sytax and also FCL base Icomparable Interface implementation using Array.Sort

---
+++ MainProgram/PetStore/AngelFish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    public abstract class AngelFish
+    {
+        private string imagepath;
+        private string title;
+        double price;
+        void addToCart() { }
+    }
+}
