4d45b061080b3cf0d0f9b272b4166ca6cc61db20
Keyur Nagvekar <106338391+Keyurnagvekar@users.noreply.github.com>
2025-07-17 14:27:10 +0530

    Arrange in Descending order

---
+++ AutomatorBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Automator
+{
+    public class AutomatorBase
+    {
+
+        public void Close() { }
+
+        public void GotoURL(string url) { }
+        public void Launch() { }
+    }
+}
---
+++ ChromeAutomatorWI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automator
+{
+    public class ChromeAutomatorWI : IBrowsable
+    {
+        public void ClickElement(string elementId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnterText(string elementId, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCurrentURL()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetElementText(string elementId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTitle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GotoURL(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsElementPresent(string elementId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Launch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
