79c4e45ad27ccd5523c0bf9407e1883b00f24469
Gayathri11-05 <gayathrithalapathi@gmail.com>
2025-07-18 16:27:10 +0530

    Binarysearch and HerkuApp Testcases Added

---
+++ MainProgram/BinarySearcherTests/Testcase_Practice.cs
@@ -0,0 +1,62 @@
+﻿using BinarySearcher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BinarySearcherTests
+{
+    /// <summary>
+    /// Contains unit tests that validate exception handling behavior
+    /// of the <see cref="BinarySearcherImpl.doBinaryDearch"/> method.
+    /// </summary>
+    [TestClass]
+    public class ExceptionTests
+    {
+        /// <summary>
+        /// Tests that <see cref="ArgumentNullException"/> is thrown when the input array is null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowsArgumentNullExceptionWhenArrayIsNull()
+        {
+            var searcher = new BinarySearcherImpl();
+            searcher.doBinaryDearch(null, 5);
+        }
+
+        /// <summary>
+        /// Tests that <see cref="RankException"/> is thrown when the input array is multidimensional.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(RankException))]
+        public void ThrowsRankExceptionWhenArrayIsMultidimensional()
+        {
+            var searcher = new BinarySearcherImpl();
+            int[,] multiArray = new int[2, 2];
+            searcher.doBinaryDearch(multiArray, 5);
+        }
+
+        /// <summary>
+        /// Tests that <see cref="InvalidOperationException"/> is thrown when an incompatible type is used for search.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ThrowsArgumentExceptionForIncompatibleType()
+        {
+            var searcher = new BinarySearcherImpl();
+            int[] array = { 1, 2, 3 };
+            searcher.doBinaryDearch(array, "string");
+        }
+
+        /// <summary>
+        /// Tests that <see cref="InvalidOperationException"/> is thrown when the value being searched does not implement a comparison mechanism.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ThrowsInvalidOperationExceptionWhenValueNotComparable()
+        {
+            var searcher = new BinarySearcherImpl();
+            object[] array = { new object(), new object() };
+            searcher.doBinaryDearch(array, new object());
+        }
+    }
+}
+
--- MainProgram/HerokuAppTests/HomePageTests.cs
+++ MainProgram/HerokuAppTests/HomePageTests.cs
@@ -9,7 +9,7 @@ namespace HerokuAppTests
         public void TitleisOK()
         {
             // Arrange
-            var expectedTitle = "Welcome to the-internet1";
+            var expectedTitle = "Welcome to the-internet";
             // Launch the browser and navigae to 
             ChromeDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
@@ -23,11 +23,36 @@ namespace HerokuAppTests
         [TestMethod]
         public void SubTitleisOK()
         {
+            // Arrange
+            var expectedSubTitle = "Available Examples";
+
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
+
+            // Act
+            IWebElement subtitleElement = driver.FindElement(By.TagName("h2"));
+            string actualSubTitle = subtitleElement.Text;
+
+            // Assert
+            Assert.AreEqual(expectedSubTitle, actualSubTitle);
+
+            driver.Quit();
         }
 
         [TestMethod]
         public void ExamplesCountis44()
         {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
+
+            // Act
+            var links = driver.FindElements(By.CssSelector("#content ul li a"));
+            int actualCount = links.Count;
+
+            // Assert
+            Assert.AreEqual(44, actualCount, "Expected 44 example links on the homepage.");
+
+            driver.Quit();
         }
     }
 }
