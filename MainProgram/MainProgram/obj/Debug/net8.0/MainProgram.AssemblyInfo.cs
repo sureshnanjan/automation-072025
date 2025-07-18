713c1028efafcd0ab4880f6a20a7f4e8be2302b5
Shreya-ascd <shreya0203.ascd@gmail.com>
2025-07-18 09:01:15 +0530

    Updated code

--- MainProgram/MainProgram/obj/Debug/net8.0/MainProgram.AssemblyInfo.cs
+++ MainProgram/MainProgram/obj/Debug/net8.0/MainProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399934c186f4d7c88c42fe8ad30c8d176b5d0dfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb0232f2cd54501ef849895ad4ca9a47da040a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
