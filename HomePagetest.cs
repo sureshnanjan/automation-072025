4b2e57f88812a8de1b88ef91257712efa2223fea
s3hwag <sehwagvijay31@gmail.com>
2025-07-21 12:30:06 +0530

    Selenium-Web-Testing

---
+++ HomePagetest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HerokuUITests
+{
+    [TestClass]
+    public class HomePageTests
+    {
+        private IWebDriver driver;
+        private const string baseUrl = "https://the-internet.herokuapp.com/";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void TitleisOK()
+        {
+            driver.Navigate().GoToUrl(baseUrl);
+            var heading = driver.FindElement(By.TagName("h1")).Text;
+            Assert.AreEqual("Welcome to the-internet", heading);
+        }
+
+        [TestMethod]
+        public void SubTitleisOK()
+        {
+            driver.Navigate().GoToUrl(baseUrl);
+            var subTitle = driver.FindElement(By.TagName("h2")).Text;
+            Assert.AreEqual("Available Examples", subTitle);
+        }
+
+        [TestMethod]
+        public void ExamplesCountis44()
+        {
+            driver.Navigate().GoToUrl(baseUrl);
+            var examples = driver.FindElements(By.CssSelector(".example a"));
+            Assert.AreEqual(44, examples.Count);
+        }
+    }
+}
