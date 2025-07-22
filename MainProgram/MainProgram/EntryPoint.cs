10e667814f0e67d7065ec9e42fceb6d191319c20
sureshnanjan <sureshnanjan@outlook.com>
2025-07-22 15:16:11 +0530

    Delegates and Lambda Expressions

--- MainProgram/MainProgram/EntryPoint.cs
+++ MainProgram/MainProgram/EntryPoint.cs
@@ -1,8 +1,15 @@
-﻿using MainProgram;
+﻿using Automator;
+using MainProgram;
 using Models;
-using Automator;
+using System.Collections.Generic;
+
+
 public class Program
 {
+    public delegate void  MyMethodwithNoArguments();
+    public delegate void MyMethodwithDayOfWeek(DayOfWeek inst);
+
+    public delegate int AMethodWith2IntsReturnInt(int a, int b);
     private static void Main(string[] args)
     {
         //Code1507();
@@ -17,12 +24,112 @@ public class Program
         //DemoUser dminstance = new DemoUser(10, "AUser");
         //DemoUser copyofOne = (DemoUser)dminstance.Clone(); // Shallow copy
 
-        GenericCalculator<int> intcal = new GenericCalculator<int>(10,20);
+        //GenericsDemo();
+
+        //GenericsDemo(args);
+        //GreetUser(DayOfWeek.Friday);
+        //GreetUser("monday");
+        //GreetUser("Holiday");
+        //MyMethodwithNoArguments method1 = delegatesDemo();
+
+        doExecution(Method1);
+        doExecution(() => Console.WriteLine("This is something different"));
+        Action<string> strOp = (arg) => Console.WriteLine($"{arg}-{arg}-{arg}");
+        strOp("suresh");
+
+        int[] mynumbers = { 1, 2, 3, 4,5,6,7 };
+        Console.WriteLine(mynumbers.Any(x => x < 100));
+        Console.WriteLine(mynumbers.All(x => x > 0));
+        Console.WriteLine(mynumbers.Aggregate((a,b)=>a*b));
+
+        SuperMan superMan = new SuperMan();
+        superMan.Play(() => { Console.WriteLine("PLaywith English"); });
+
+       
+
+
+        
+
+
+
+    }
+
+    private static MyMethodwithNoArguments delegatesDemo()
+    {
+        MyMethodwithNoArguments method1 = designToInterface;
+        MyMethodwithDayOfWeek method2 = GreetUser;
+
+        GreetUser(DayOfWeek.Friday);
+        method2(DayOfWeek.Monday);
+        method2 = dayofweek => Console.WriteLine($"Doing something different with args {dayofweek.ToString()}");
+        method2(DayOfWeek.Sunday);
+        method1 = () => Console.WriteLine("This is a delegate"); // Lambda 
+        AMethodWith2IntsReturnInt adder = addTwoNumbers;
+        adder(100, 200);
+
+        AMethodWith2IntsReturnInt doubler = (first, second) => (first * 2) + (second * 2);
+        Action<int, int, int> threeargs;
+        threeargs = (a, b, c) => Console.WriteLine((a * 2) + (b * 2) + (c * 2));
+        return method1;
+    }
+
+    private static void GenericsDemo(string[] args)
+    {
+        Console.WriteLine($"Welcome to Automation");
+        string browsername = args[0];
+        switch (browsername)
+        {
+            case "chrome":
+                Console.WriteLine("Running in chrome");
+                break;
+            case "firefox":
+                Console.WriteLine("Running on Firefox");
+                break;
+            default:
+                Console.WriteLine("This browser not supported");
+                break;
+        }
+    }
+
+    static void Method1() {
+
+        Console.WriteLine("Inside Method 1");    
+    }
+
+    static void doExecution(Action argfunction) {
+        Console.WriteLine("Executing another Method");
+        argfunction();
+        Console.WriteLine("Doing something after execution");
+    }
+
+    private static void GenericsDemo()
+    {
+        GenericCalculator<int> intcal = new GenericCalculator<int>(10, 20);
         GenericCalculator<float> floatcal = new GenericCalculator<float>(10.5f, 20.5f);
         GenericCalculator<DemoUser> demUsercal = new GenericCalculator<DemoUser>(new DemoUser(10, "AUser"), new DemoUser(20, "BUser"));
         List<DemoUser> myUsers = new List<DemoUser>();
         List<int> users = new List<int>();
+    }
 
+    private static void GreetUser(DayOfWeek dayofweek) {
+        switch (dayofweek)
+        {
+            case DayOfWeek.Monday:
+                Console.WriteLine();
+                break;
+            case DayOfWeek.Tuesday:
+                Console.WriteLine();
+                break;
+            case DayOfWeek.Wednesday:
+                Console.WriteLine();
+                break;
+
+            default:
+                Console.WriteLine("Everything else");
+                break;
+        }
+        Console.WriteLine("You are in the nth Day of the week");
+        Console.WriteLine($"Hello User Have a great {dayofweek}");
     }
 
     private static void designToInterface()
@@ -166,4 +273,6 @@ public class Program
         float myFloat = 10.5f; // Float literal
         bool isTrue = true; // Boolean literal
     }
+
+
 }
