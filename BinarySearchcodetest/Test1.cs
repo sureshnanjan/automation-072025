f8a320495aaf8d9cff43b210815d8de18b07b309
Keyurnagvekar <keyurnagvekar03@gmail.com>
2025-07-29 10:30:03 +0530

    Array Methods and classes

---
+++ BinarySearchcodetest/Test1.cs
@@ -0,0 +1,18 @@
+﻿namespace BinarySearchcodetest
+{
+    [TestClass]
+    public class ArraySortTest
+    {
+        [TestMethod]
+        public void DefaultSortingtest()
+        {
+            // Arrange
+            int[] array = { 5, 3, 8, 1, 2 };
+            int[] expected = { 1, 2, 3, 5, 8 };
+            // Act
+            Array.Sort(array);
+            // Assert
+            CollectionAssert.AreEqual(expected, array);
+        }
+    }
+}
---
+++ IComparer/Test1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System;
+
+namespace ReverseSortExample.Tests
+{
+    [TestClass]
+    public class SortTests
+    {
+        private class ReverserClass : IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                return StringComparer.InvariantCultureIgnoreCase.Compare(y, x);
+            }
+        }
+
+        [TestMethod]
+        public void DefaultSort_ShouldBeAscending_CaseSensitive()
+        {
+            string[] input = { "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+            string[] expected = { "brown", "dog", "fox", "jumps", "lazy", "over", "quick", "the", "The" };
+
+            Array.Sort(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void ReverseSort_ShouldBeDescending_CaseInsensitive()
+        {
+            string[] input = { "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+            string[] expected = { "the", "The", "quick", "over", "lazy", "jumps", "fox", "dog", "brown" };
+
+            Array.Sort(input, new ReverserClass());
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+    }
+}
