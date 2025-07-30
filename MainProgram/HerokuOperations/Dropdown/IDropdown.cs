ced68f2bb6f4acf8c63d940708a12219aade2466
Keyur Nagvekar <106338391+Keyurnagvekar@users.noreply.github.com>
2025-07-30 20:52:20 +0530

    Update IDropdown.cs

--- MainProgram/HerokuOperations/Dropdown/IDropdown.cs
+++ MainProgram/HerokuOperations/Dropdown/IDropdown.cs
@@ -1,37 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
+using OpenQA.Selenium;              // For IWebDriver and IWebElement
+using OpenQA.Selenium.Chrome;       // For ChromeDriver
+using OpenQA.Selenium.Support.UI;   // For WebDriverWait and SelectElement
+using System;                       // For TimeSpan
 
-namespace DropdownTests
-{
-    [TestClass]
-    public class DropdownTest
-    {
-        private IWebDriver driver;
-        private WebDriverWait wait;
-
-        [TestInitialize]
-        public void Setup()
-        {
-            // Arrange: Setup browser
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dropdown");
-            driver.Manage().Window.Maximize();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        }
-
-        [TestMethod]
-        public void SelectDropdownOption1()
-        {  }
-
-        [TestMethod]
-        public void SelectDropdownOption2()
-        { }
-
-        [TestCleanup]
-        public void TearDown()
-        {}
-    }
-}
+// Parameters required for dropdown testing using Selenium
+private IWebDriver driver;          // Controls the browser instance (Chrome, Firefox, etc.)
+private WebDriverWait wait;         // Provides explicit wait functionality for specific elements or conditions
+private IWebElement dropdown;       // Represents the <select> HTML element on the page
+private SelectElement select;       // Wrapper around the <select> element to simplify dropdown interactions
