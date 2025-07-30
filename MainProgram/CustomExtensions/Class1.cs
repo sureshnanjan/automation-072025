3b7e32d08bbfa8ac6a1c3505fa0e3b2920db4823
Sivasj@4321 <itssivasj@gmail.com>
2025-07-30 16:59:15 +0530

    Interfaces for Idownloader,IRedirect,IFileUploader

---
+++ MainProgram/CustomExtensions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace CustomExtensions
+{
+    public static class MyExtensions
+    {
+
+        public static string AddSalutaion(this string arg) { return String.Empty; }
+        public static string AddSalutaion(this Array arg) { return String.Empty;  }
+
+    }
+}
