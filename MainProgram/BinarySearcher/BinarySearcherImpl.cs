de1f977452293ae63cd4a559548082958e4fecd9
s3hwag <sehwagvijay31@gmail.com>
2025-07-30 10:59:11 +0530

    Updated with trainer/suresh

--- MainProgram/BinarySearcher/BinarySearcherImpl.cs
+++ MainProgram/BinarySearcher/BinarySearcherImpl.cs
@@ -1,9 +1,42 @@
 ﻿namespace BinarySearcher
 {
+    /// <summary>
+    /// This is the implementation of a binary search algorithm.
+    /// </summary>
     public class BinarySearcherImpl
     {
+        /// <summary>
+        /// Performs a binary search on the given array for the specified key.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public int doBinaryDearch(Array input, Object key) {
             return Array.BinarySearch(input, key);
+        }
+
+        // Code added just for Reference . Please ignore this
+        public int doBinarySearch(List<int> input, int key)
+        {
+            int low = 0;
+            int high = input.Count - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (input[mid] == key)
+                {
+                    return mid; // Key found
+                }
+                else if (input[mid] < key)
+                {
+                    low = mid + 1; // Search in the right half
+                }
+                else
+                {
+                    high = mid - 1; // Search in the left half
+                }
+            }
+            return -1; // Key not found
         }
 
     }
---
+++ MainProgram/HerokuAppTests/HomePageTests.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+namespace HerokuAppTests
+{
+    [TestClass]
+    public sealed class HomePageTests
+    {
+        [TestInitialize]
+        public void Init() {
+            // Read from app.config
+            //AppContext appContext = Confi
+           
+        }
+        [TestMethod]
+        public void TitleisOK()
+        {
+            // Arrange
+            var expectedTitle = "Welcome to the-internet1";
+            // Launch the browser and navigae to 
+            ISearchContext driver = new ChromeDriver();
+            driver = new FirefoxDriver();
+            ((IWebDriver)driver).Navigate().GoToUrl("https://the-internet.herokuapp.com/");
+            IWebElement pageheading = driver.FindElement(By.TagName("h1"));
+            // 
+            // Act
+            var actualTitle = pageheading.Text;
+            // Assert
+            Assert.AreEqual(expectedTitle, actualTitle);
+        }
+        [TestMethod]
+        public void SubTitleisOK()
+        {
+        }
+
+        [TestMethod]
+        public void ExamplesCountis44()
+        {
+        }
+    }
+}
