00b5b8171e463816d0bfe32401d35b6322fd5773
Gayathri11-05 <gayathrithalapathi@gmail.com>
2025-07-30 16:54:49 +0530

    Adding Interface

---
+++ MainProgram/HerokuOperations/ICheckBoxes.cs
@@ -0,0 +1,53 @@
+﻿/*
+* Copyright (c) 2025 Gayathri Thalapathi
+* 
+* This interface is part of the HerokuOperations project.
+* It defines the contract for interacting with the "Checkboxes" page
+* on the-internet.herokuapp.com.
+* 
+* Licensed under the MIT License. You may obtain a copy of the License at
+* https://opensource.org/licenses/MIT
+*/
+
+using System.Collections.Generic;
+
+namespace HerokuOperations
+{
+    /// <summary>
+    /// Represents actions and state checks for the Checkboxes page
+    /// on https://the-internet.herokuapp.com/checkboxes.
+    /// </summary>
+    public interface ICheckBoxes
+    {
+        /// <summary>
+        /// Checks the first checkbox if it is not already checked.
+        /// </summary>
+        void CheckFirstBox();
+
+        /// <summary>
+        /// Unchecks the second checkbox if it is currently checked.
+        /// </summary>
+        void UncheckSecondBox();
+
+        /// <summary>
+        /// Determines whether the first checkbox is checked.
+        /// </summary>
+        /// <returns>True if checked; otherwise, false.</returns>
+        bool IsFirstBoxChecked();
+
+        /// <summary>
+        /// Determines whether the second checkbox is checked.
+        /// </summary>
+        /// <returns>True if checked; otherwise, false.</returns>
+        bool IsSecondBoxChecked();
+
+        /// <summary>
+        /// Retrieves the checked state of all checkboxes on the page.
+        /// </summary>
+        /// <returns>
+        /// A list of booleans indicating checkbox states:
+        /// true = checked, false = unchecked.
+        /// </returns>
+        List<bool> GetAllCheckboxStates();
+    }
+}
---
+++ MainProgram/HerokuOperations/IWebDriver.cs
@@ -0,0 +1,6 @@
+﻿namespace HerokuOperations
+{
+    public interface IWebDriver
+    {
+    }
+}
