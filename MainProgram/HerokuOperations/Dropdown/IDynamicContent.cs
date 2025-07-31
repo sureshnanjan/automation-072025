ab8bc1e01114702b9af6bc4e215bb64e49e57173
Keyur Nagvekar <106338391+Keyurnagvekar@users.noreply.github.com>
2025-07-31 10:45:35 +0530

    Update IDynamicContent.cs

--- MainProgram/HerokuOperations/Dropdown/IDynamicContent.cs
+++ MainProgram/HerokuOperations/Dropdown/IDynamicContent.cs
@@ -1,9 +1,12 @@
-using OpenQA.Selenium;              // For IWebDriver and IWebElement
-using OpenQA.Selenium.Chrome;       // For ChromeDriver
-using OpenQA.Selenium.Support.UI;   // For WebDriverWait and SelectElement
-using System;                       // For TimeSpan
+namespace HerokuOperations
+{
+    public interface IDynamicContentPage
+    {
+        string GetTitle();
+        string GetSubTitle();
+        int GetRowCount();
 
-IWebDriver driver;                  // Controls the browser instance (Chrome, Firefox, etc.)
-WebDriverWait wait;                // Provides explicit wait functionality for specific elements or conditions
-IWebElement dropdown;              // Represents the <select> HTML element on the page
-SelectElement select;              // Wrapper around the <select> element to simplify dropdown interactions
+        string GetTextFromRow(int rowIndex);
+        string GetImageSourceFromRow(int rowIndex);
+    }
+}
