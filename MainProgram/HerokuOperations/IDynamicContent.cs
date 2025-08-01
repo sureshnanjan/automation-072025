4fe275821ae18ce7fc37fffaffafdabe901ac927
Keyurnagvekar <keyurnagvekar03@gmail.com>
2025-08-01 15:09:52 +0530

    test cases

--- MainProgram/HerokuOperations/IDynamicContent.cs
+++ MainProgram/HerokuOperations/IDynamicContent.cs
@@ -1,71 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System;
-using System.Threading;
-using HerokuOperations;
-
-namespace HerokuTests
+﻿namespace HerokuOperations
 {
-    [TestClass]
-    public class DynamicControlsTests
+    public interface IDynamicContentPage
     {
-        private IWebDriver driver;
-        private IDynamicControlsPage page;
+        string GetTitle();
+        string GetSubTitle();
+        int GetRowCount();
 
-        [TestInitialize]
-        public void Setup()
-        {
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dynamic_controls");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
-            page = new DynamicControlsPage(driver); // <-- Replace with your concrete implementation
-        }
-
-        [TestCleanup]
-        public void Teardown()
-        {
-            driver.Quit();
-        }
-
-        // --- Checkbox Section Tests ---
-
-        [TestMethod]
-        public void TestCheckboxRemoveAndAdd()
-        {
-            if (page.IsCheckboxDisplayed())
-            {
-                page.ClickRemoveOrAddButton();
-                Thread.Sleep(3000); // Wait for AJAX animation
-                Assert.IsFalse(page.IsCheckboxDisplayed(), "Checkbox should be removed.");
-                Assert.AreEqual("It's gone!", page.GetCheckboxMessage());
-            }
-
-            page.ClickRemoveOrAddButton();
-            Thread.Sleep(3000); // Wait for checkbox to reappear
-            Assert.IsTrue(page.IsCheckboxDisplayed(), "Checkbox should be added back.");
-            Assert.AreEqual("It's back!", page.GetCheckboxMessage());
-        }
-
-        // --- Input Field Section Tests ---
-
-        [TestMethod]
-        public void Test_Input_Enable_And_Disable()
-        {
-            if (!page.IsInputEnabled())
-            {
-                page.ClickEnableOrDisableButton();
-                Thread.Sleep(3000); // Wait for enable
-                Assert.IsTrue(page.IsInputEnabled(), "Input should be enabled.");
-                Assert.AreEqual("It's enabled!", page.GetInputMessage());
-            }
-
-            page.EnterText("Test input");
-            page.ClickEnableOrDisableButton();
-            Thread.Sleep(3000); // Wait for disable
-            Assert.IsFalse(page.IsInputEnabled(), "Input should be disabled.");
-            Assert.AreEqual("It's disabled!", page.GetInputMessage());
-        }
+        string GetTextFromRow(int rowIndex);
+        string GetImageSourceFromRow(int rowIndex);
     }
 }
