3baaee811d3e6554f11569462b75e58ffd9a0e35
Keyur Nagvekar <106338391+Keyurnagvekar@users.noreply.github.com>
2025-08-01 13:34:10 +0530

    Update IShiftingContent.cs

--- MainProgram/HerokuOperations/IShiftingContent.cs
+++ MainProgram/HerokuOperations/IShiftingContent.cs
@@ -1,43 +1,11 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using HerokuOperations;
-using System;
-
-namespace HerokuTests
+namespace HerokuOperations
 {
-    [TestClass]
-    public class DynamicControlsTests
+    public interface IShiftingContentPage
     {
-        private IWebDriver driver;
-        private IDynamicControlsPage controls;
+        string GetTitle();
+        string GetDescription();
 
-        [TestInitialize]
-        public void Setup()
-        {
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dynamic_controls");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            controls = new DynamicControlsPage(driver); // ? Replace with your actual class
-        }
-
-        [TestCleanup]
-        public void Teardown() => driver.Quit();
-
-        [TestMethod]
-        public void Test_CheckboxToggle()
-        {
-            controls.ClickRemoveOrAddButton();
-            string message = controls.GetCheckboxMessage();
-            Assert.IsTrue(message.Contains("gone") || message.Contains("added"));
-        }
-
-        [TestMethod]
-        public void Test_InputEnableDisable()
-        {
-            controls.ClickEnableOrDisableButton();
-            bool enabled = controls.IsInputEnabled();
-            Assert.IsTrue(enabled || !enabled);
-        }
+        string[] GetAllLinkTexts();
+        int GetLinkCount();
     }
 }
