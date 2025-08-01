fc00df1b9fa622e2a8555939fc31088ce404da4d
Keyur Nagvekar <106338391+Keyurnagvekar@users.noreply.github.com>
2025-08-01 14:37:29 +0530

    Update IDynamicControlmethod.cs

--- MainProgram/HerokuAppScenarios/IDynamicControlmethod.cs
+++ MainProgram/HerokuAppScenarios/IDynamicControlmethod.cs
@@ -1,3 +1,13 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DynamicControlsPageTests.cs" company="Keyur Nagvekar">
+//   Copyright (c) 2025 Keyur Nagvekar. All rights reserved.
+//   This file provides automated MSTest test cases for verifying dynamic interactions
+//   with checkbox and input controls on the Dynamic Controls page using the
+//   DynamicControlsPage class.
+//   Redistribution or modification of this file is subject to author permissions.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -5,6 +15,11 @@ using HerokuOperations;
 
 namespace HerokuOperationsTests
 {
+    /// <summary>
+    /// Contains automated test cases for the DynamicControlsPage class, 
+    /// which interacts with dynamic checkbox and input elements on 
+    /// https://the-internet.herokuapp.com/dynamic_controls.
+    /// </summary>
     [TestClass]
     public class DynamicControlsPageTests
     {
@@ -28,63 +43,51 @@ namespace HerokuOperationsTests
         [TestMethod]
         public void Checkbox_CanBeRemovedAndAdded()
         {
-            // Arrange
             bool initiallyPresent = dynamicControls.IsCheckboxDisplayed();
 
-            // Act - Remove Checkbox
             dynamicControls.ClickRemoveOrAddButton();
             bool isRemoved = !dynamicControls.IsCheckboxDisplayed();
             string removeMsg = dynamicControls.GetCheckboxMessage();
 
-            // Assert
             Assert.IsTrue(isRemoved, "Checkbox should be removed.");
-            Assert.AreEqual("It's gone!", removeMsg, "Remove message mismatch.");
+            Assert.AreEqual("It's gone!", removeMsg);
 
-            // Act - Add Checkbox back
             dynamicControls.ClickRemoveOrAddButton();
             bool isReadded = dynamicControls.IsCheckboxDisplayed();
             string addMsg = dynamicControls.GetCheckboxMessage();
 
-            // Assert
             Assert.IsTrue(isReadded, "Checkbox should be added back.");
-            Assert.AreEqual("It's back!", addMsg, "Add message mismatch.");
+            Assert.AreEqual("It's back!", addMsg);
         }
 
         [TestMethod]
         public void Input_CanBeEnabledAndDisabled()
         {
-            // Act - Enable input
             dynamicControls.ClickEnableOrDisableButton();
             bool isEnabled = dynamicControls.IsInputEnabled();
             string enableMsg = dynamicControls.GetInputMessage();
 
-            // Assert
-            Assert.IsTrue(isEnabled, "Input field should be enabled.");
-            Assert.AreEqual("It's enabled!", enableMsg, "Enable message mismatch.");
+            Assert.IsTrue(isEnabled);
+            Assert.AreEqual("It's enabled!", enableMsg);
 
-            // Act - Disable input
             dynamicControls.ClickEnableOrDisableButton();
             bool isDisabled = !dynamicControls.IsInputEnabled();
             string disableMsg = dynamicControls.GetInputMessage();
 
-            // Assert
-            Assert.IsTrue(isDisabled, "Input field should be disabled.");
-            Assert.AreEqual("It's disabled!", disableMsg, "Disable message mismatch.");
+            Assert.IsTrue(isDisabled);
+            Assert.AreEqual("It's disabled!", disableMsg);
         }
 
         [TestMethod]
         public void Input_CanAcceptTextWhenEnabled()
         {
-            // Arrange - Enable input
             dynamicControls.ClickEnableOrDisableButton();
-            Assert.IsTrue(dynamicControls.IsInputEnabled(), "Input should be enabled to send keys.");
+            Assert.IsTrue(dynamicControls.IsInputEnabled());
 
-            // Act
             dynamicControls.EnterText("Hello Test");
             string inputValue = driver.FindElement(By.XPath("//form[@id='input-example']/input")).GetAttribute("value");
 
-            // Assert
-            Assert.AreEqual("Hello Test", inputValue, "Input text mismatch.");
+            Assert.AreEqual("Hello Test", inputValue);
         }
     }
 }
