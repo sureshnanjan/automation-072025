067499370bfbcaa7d9c475719a918e57e3f85d8c
Vamsi2359 <vamsikrishna.k2359@gmail.com>
2025-08-01 11:12:34 +0530

    .

--- MainProgram/HerokuOperations/IInfinity.cs
+++ MainProgram/HerokuOperations/IInfinity.cs
@@ -1,27 +1,47 @@
-﻿namespace HerokuOperations
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuOperations
 {
-    // Interface for Infinite Scroll Page interactions
-    public interface IInfinity
+    public interface IinfiniteScroll
     {
         // Navigates to the Infinite Scroll page
         void GotoPage();
 
-        // Scrolls down the page by a given number of times
-        void ScrollDown(int times);
+        // Returns the title of the page
+        string GetTitle();
 
-        // Returns the number of paragraph blocks loaded on the page
-        int GetParagraphCount();
+        // Returns the subtitle of the page
+        string GetSubTitle();
 
-        // Gets the current vertical scroll position (Y-axis)
-        int GetScrollY();
-
-        // Scrolls to the bottom of the page
+        // Scrolls all the way to the bottom of the page
         void ScrollToBottom();
 
-        // Scrolls to the top of the page
+        // Returns the total scrollable height of the page
+        int GetScrollHeight();
+
+        // Returns the total scrollable width of the page
+        int GetScrollWidth();
+
+        // Scrolls back to the top of the page
         void ScrollToTop();
 
-        // Closes the browser session
-        void Close();
+        // Scrolls all the way to the left side of the page
+        void ScrollToLeft();
+
+        // Scrolls all the way to the right side of the page
+        void ScrollToRight();
+
+        // Returns the current horizontal scroll position
+        int GetScrollX();
+
+        // Returns the current vertical scroll position
+        int GetScrollY();
+
+        // Scrolls to a specific X and Y coordinate on the page
+        void ScrollTo(int x, int y);
     }
 }
