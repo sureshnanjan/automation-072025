fb165764676517af65372fa73178bb74e3bce879
Shreya-ascd <shreya0203.ascd@gmail.com>
2025-08-01 11:36:28 +0530

    Testcases for FileDownload RedirectLink are included

--- MainProgram/HerokuAppScenarios/FileDownloaderScenarios.cs
+++ MainProgram/HerokuAppScenarios/FileDownloaderScenarios.cs
@@ -1,41 +1,74 @@
-﻿using HerokuOperations;
+﻿/*******************************************************
+* Copyright (c) 2025, Shreya S G
+* All rights reserved.
+* 
+* File: FileDownloaderScenarios.cs
+* Purpose: Test scenarios for verifying file download functionality 
+*          in the HerokuApp "File Downloader" page.
+* 
+* Description:
+* This file contains NUnit test cases for the `IDownloader` interface 
+* in the `HerokuOperations` namespace. It tests page title retrieval, 
+* file count retrieval, and the list of available file names.
+*******************************************************/
 
+using System;
+using System.Linq;
+using HerokuOperations;
+using NUnit.Framework;
 
-namespace HerokuAppScenarios;
-
-public class FileDownloaderScenarios
+namespace HerokuAppScenarios
 {
-    [SetUp]
-    public void Setup()
+    /// <summary>
+    /// Test scenarios for verifying file download functionality 
+    /// on the HerokuApp File Downloader page.
+    /// </summary>
+    public class FileDownloaderScenarios
     {
+        private IDownloader downloader;
+
+        /// <summary>
+        /// Initializes resources before each test.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            // TODO: Initialize a concrete or mocked implementation of IDownloader
+            // Example: downloader = new FileDownloader(driver);
+        }
+
+        /// <summary>
+        /// Verifies that the page title for the File Downloader page is correct.
+        /// </summary>
+        [Test]
+        public void FileDownLoadIsTitleOk()
+        {
+            string expected = "File Downloader";
+            string actual = downloader.GetFileDownloadTitle();
+            Assert.AreEqual(expected, actual, "The page title does not match the expected value.");
+        }
+
+        /// <summary>
+        /// Verifies that the number of downloadable files returned is correct.
+        /// </summary>
+        [Test]
+        public void GetCountOfFiles_ReturnsCorrectCount()
+        {
+            int expectedCount = 1;
+            int actualCount = downloader.GetCountOfFiles();
+            Assert.AreEqual(expectedCount, actualCount, "The count of downloadable files is incorrect.");
+        }
+
+        /// <summary>
+        /// Verifies that the list of file names returned is correct.
+        /// </summary>
+        [Test]
+        public void GetAllFileNames_ReturnsFileNames()
+        {
+            var fileNames = downloader.GetAllFileNames();
+            Assert.IsNotNull(fileNames, "The list of file names should not be null.");
+            Assert.AreEqual(1, fileNames.Count, "Unexpected number of file names returned.");
+            Assert.AreEqual("testfile.txt", fileNames.First(), "The first file name does not match the expected value.");
+        }
     }
-
-    [Test]
-    public void FileDownLoadIsTitleOk()
-    {
-        string expected = "File Downloader";
-        IDownloader downloader;
-        string actual = downloader.GetFileDownloadTitle();
-        Assert.AreEqual(expected, actual);
-    }
-
-    [Test]
-    public void GetCountOfFiles_ReturnsCorrectCount()
-    {
-        int expectedCount = 1;
-        int actualCount = downloader.GetCountOfFiles();
-        Assert.AreEqual(expectedCount, actualCount);
-    }
-
-    [Test]
-    public void GetAllFileNames_ReturnsFileNames()
-    {
-        var fileNames = downloader.GetAllFileNames();
-        Assert.IsNotNull(fileNames);
-        Assert.AreEqual(1, fileNames.Count);
-        Assert.AreEqual("testfile.txt", fileNames.First());
-    }
-
-}
-
 }
--- MainProgram/HerokuAppScenarios/RedirectScenarios.cs
+++ MainProgram/HerokuAppScenarios/RedirectScenarios.cs
@@ -1,52 +1,100 @@
-﻿using NUnit.Framework;
+﻿/*******************************************************
+* Copyright (c) 2025, Shreya S G
+* All rights reserved.
+* 
+* File: RedirectScenarios.cs
+* Purpose: Test scenarios for verifying redirection functionality 
+*          in the HerokuApp "Redirector" page.
+* 
+* Description:
+* This file contains NUnit test cases for the `IRedirect` interface 
+* in the `HerokuOperations` namespace. It tests the page title, 
+* paragraph content, redirection link, and final redirected page.
+*******************************************************/
+
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using HerokuOperations;
 
 namespace HerokuAppScenarios
 {
+    /// <summary>
+    /// Test scenarios for verifying redirection functionality
+    /// on the HerokuApp Redirector page.
+    /// </summary>
     [TestFixture]
     public class RedirectScenarios
     {
-        
+        private IWebDriver driver;
+        private IRedirect redirect;
 
+        /// <summary>
+        /// Initializes the Chrome driver and page object before each test.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize Chrome WebDriver
+            driver = new ChromeDriver();
+
+            // Navigate to the redirector page
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/redirector");
+
+            // TODO: Initialize the concrete implementation of IRedirect
+            // Example: redirect = new RedirectPage(driver);
+        }
+
+        /// <summary>
+        /// Closes the browser after each test.
+        /// </summary>
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+
+        /// <summary>
+        /// Verifies that the page title of the Redirector page is correct.
+        /// </summary>
         [Test]
         public void GetTitle_ShouldReturnCorrectText()
         {
-            IRedirect redirect;
             string title = redirect.GetTitle();
-            Assert.That(title, Is.EqualTo("Redirection"));
+            Assert.That(title, Is.EqualTo("Redirection"), "The page title does not match the expected value.");
         }
 
-        [Test]        
+        /// <summary>
+        /// Verifies that the paragraph text on the Redirector page contains the keyword 'redirect'.
+        /// </summary>
+        [Test]
         public void GetParagraphText_ShouldContainExpectedMessage()
         {
             string paragraph = redirect.GetParagraphText();
-            StringAssert.Contains("redirect", paragraph); // keyword match
+            StringAssert.Contains("redirect", paragraph, "The paragraph does not contain the expected keyword.");
         }
 
+        /// <summary>
+        /// Verifies that clicking the 'Click here' link redirects to the Status Codes page.
+        /// </summary>
         [Test]
         public void ClickhereLink_ShouldRedirectToStatusCodesPage()
         {
             redirect.ClickhereLink();
-            Assert.That(driver.Url, Does.Contain("/status_codes"));
+            Assert.That(driver.Url, Does.Contain("/status_codes"), "The page did not redirect to the Status Codes page.");
         }
 
-        
+        /*
+         *  Test Cases (to be implemented):
+         * 
+         * 1. GetTitle() - Validate heading of the Redirector page.
+         * 2. GetContent() - Verify the content of the page.
+         * 3. GetLogo() - Check if a logo is present.
+         * 4. ClickHere() - Verify redirection to the next page:
+         *    4.1. Clickhere() - Should redirect to the next page.
+         *    4.2. GetTitleStatusCode() - Validate the title of the Status Codes page.
+         *    4.3. GetContentStatusCode() - Validate the paragraph content.
+         *    4.4. Get200() - Verify navigation to the 200 Status Code page.
+         */
     }
 }
-
-
-/*
- Type of Testcases to be written
-1 . GetTitle() - heading (Redirect link)
-2. GetContent() - get the content of the  page
-3. GetLogo() - if logo present or not .
-4.ClickHere() - link to redirect to next page.
-  4.1 Clickhere() - should redirect to the next page
-  4.2 GetTitleStatusCode() - title of status code
-  4.3 GetContentStatusCode() - paragraph 
-  4.4 Get200() - redirect to 200 content page
-
-
- * \
