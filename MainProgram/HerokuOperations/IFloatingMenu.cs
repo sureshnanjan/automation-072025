cc893ce3e51a2281c09df64285897e3cb3749290
Sowmyasri24 <srisowmya2003@gmail.com>
2025-08-01 20:40:02 +0530

    IFloatingMenuPage updated

--- MainProgram/HerokuOperations/IFloatingMenu.cs
+++ MainProgram/HerokuOperations/IFloatingMenu.cs
@@ -18,43 +18,54 @@ namespace HerokuOperations
     /// <summary>
     /// Defines operations for interacting with the Floating Menu page.
     /// </summary>
-    public interface IFloatingMenu
+    public interface IFloatingMenuPage
     {
         /// <summary>
-        /// Checks if the floating menu is visible on the screen.
+        /// Checks if a specified menu item (e.g., "Home", "About") is visible on the page.
         /// </summary>
-        /// <returns>True if the menu is visible; otherwise, false.</returns>
-        bool IsMenuVisible();
+        /// <param name="menuItem">The name of the menu item.</param>
+        /// <returns>True if the menu item is visible; otherwise, false.</returns>
+        bool IsMenuVisible(string menuItem);
 
         /// <summary>
-        /// Clicks on the "Home" link in the floating menu.
-        /// </summary>
-        void ClickHome();
-
-        /// <summary>
-        /// Clicks on the "News" link in the floating menu.
-        /// </summary>
-        void ClickNews();
-
-        /// <summary>
-        /// Clicks on the "Contact" link in the floating menu.
-        /// </summary>
-        void ClickContact();
-
-        /// <summary>
-        /// Clicks on the "About" link in the floating menu.
-        /// </summary>
-        void ClickAbout();
-
-        /// <summary>
-        /// Scrolls the page to the bottom (to test if the menu still floats).
+        /// Scrolls the page to the bottom to test floating menu visibility.
         /// </summary>
         void ScrollToBottom();
 
         /// <summary>
-        /// Gets the current Y position of the floating menu (to verify if it's fixed).
+        /// Scrolls the page to the middle height to verify if the menu remains visible.
         /// </summary>
-        /// <returns>The vertical (Y-axis) pixel position of the menu.</returns>
-        int GetMenuYPosition();
+        void ScrollToMiddle();
+
+        /// <summary>
+        /// Checks whether the floating menu remains visible after scrolling.
+        /// </summary>
+        /// <returns>True if the menu is still visible; otherwise, false.</returns>
+        bool IsFloatingMenuStillVisible();
+
+        /// <summary>
+        /// Clicks a specific floating menu item and returns the ID of the section navigated to.
+        /// </summary>
+        /// <param name="menuItem">The menu item to click (e.g., "News").</param>
+        /// <returns>The section ID the page navigated to (e.g., "news").</returns>
+        string ClickMenu(string menuItem);
+
+        /// <summary>
+        /// Gets the main heading text of the floating menu page.
+        /// </summary>
+        /// <returns>The page heading (e.g., "Floating Menu").</returns>
+        string GetHeading();
+
+        /// <summary>
+        /// Retrieves all paragraph elements present on the page.
+        /// </summary>
+        /// <returns>A list or collection of paragraph texts or elements.</returns>
+        IList<string> GetParagraphs();
+
+        /// <summary>
+        /// Checks whether the floating menu can be navigated using keyboard input (e.g., Tab key).
+        /// </summary>
+        /// <returns>True if the floating menu is keyboard-accessible; otherwise, false.</returns>
+        bool CanAccessMenuWithKeyboard();
     }
 }
