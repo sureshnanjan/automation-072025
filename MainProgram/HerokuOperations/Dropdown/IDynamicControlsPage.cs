ce5134e41d34d3f2a192922af88ed26153228465
Keyurnagvekar <keyurnagvekar03@gmail.com>
2025-08-01 10:49:54 +0530

    .

--- MainProgram/HerokuOperations/Dropdown/IDynamicControlsPage.cs
+++ MainProgram/HerokuOperations/Dropdown/IDynamicControlsPage.cs
@@ -1,71 +1,75 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using HerokuOperations;
 using System;
 
 namespace HerokuTests
-
 {
     [TestClass]
     public class DynamicControlsTests
     {
-        // The error CS0101 indicates that there are multiple definitions of the 'DynamicControlsTests' class in the same namespace 'HerokuTests'.  
-        // To fix this, ensure that there is only one definition of the 'DynamicControlsTests' class in the namespace.  
-        // If there is another file with the same class definition, rename or remove the duplicate class.  
-
-        // Assuming this file contains the correct implementation, you should check other files in the 'HerokuTests' namespace for duplicate definitions.  
-        // If you find a duplicate, either rename the class or remove it if it is not needed.  
-
-        // If you cannot locate the duplicate, please provide additional files or information for further assistance.
         private IWebDriver driver;
-        private IDynamicControlsPage page;
 
         [TestInitialize]
         public void Setup()
         {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dynamic_controls");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
 
-            page = new DynamicControlsPage(driver); // Your implementation of the interface
+        [TestMethod]
+        public void Test_RemoveAndAddCheckbox()
+        {
+            var removeButton = driver.FindElement(By.XPath("//form[@id='checkbox-example']//button"));
+            removeButton.Click();
+
+            var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => !IsElementPresent(By.Id("checkbox")));
+
+            Assert.IsFalse(IsElementPresent(By.Id("checkbox")), "Checkbox should be removed.");
+
+            var addButton = driver.FindElement(By.XPath("//form[@id='checkbox-example']//button"));
+            addButton.Click();
+
+            wait.Until(d => IsElementPresent(By.Id("checkbox")));
+            Assert.IsTrue(IsElementPresent(By.Id("checkbox")), "Checkbox should be added back.");
+        }
+
+        [TestMethod]
+        public void Test_EnableAndDisableTextbox()
+        {
+            var enableButton = driver.FindElement(By.XPath("//form[@id='input-example']//button"));
+            enableButton.Click();
+
+            var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.FindElement(By.XPath("//form[@id='input-example']/input")).Enabled);
+
+            Assert.IsTrue(driver.FindElement(By.XPath("//form[@id='input-example']/input")).Enabled, "Textbox should be enabled.");
+
+            enableButton = driver.FindElement(By.XPath("//form[@id='input-example']//button"));
+            enableButton.Click();
+
+            wait.Until(d => !d.FindElement(By.XPath("//form[@id='input-example']/input")).Enabled);
+            Assert.IsFalse(driver.FindElement(By.XPath("//form[@id='input-example']/input")).Enabled, "Textbox should be disabled.");
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
         }
 
         [TestCleanup]
         public void Teardown()
         {
             driver.Quit();
-        }
-
-        [TestMethod]
-        public void Checkbox_Remove_Then_Add()
-        {
-            // Remove
-            page.ClickRemoveOrAddButton();
-            Assert.IsFalse(page.IsCheckboxDisplayed(), "Checkbox should be removed.");
-            Assert.AreEqual("It's gone!", page.GetCheckboxMessage());
-
-            // Add
-            page.ClickRemoveOrAddButton();
-            Assert.IsTrue(page.IsCheckboxDisplayed(), "Checkbox should be added back.");
-            Assert.AreEqual("It's back!", page.GetCheckboxMessage());
-        }
-
-        [TestMethod]
-        public void Input_Enable_Then_Disable()
-        {
-            // Enable
-            page.ClickEnableOrDisableButton();
-            Assert.IsTrue(page.IsInputEnabled(), "Input field should be enabled.");
-            Assert.AreEqual("It's enabled!", page.GetInputMessage());
-
-            // Type text
-            page.EnterText("Selenium Test");
-
-            // Disable
-            page.ClickEnableOrDisableButton();
-            Assert.IsFalse(page.IsInputEnabled(), "Input field should be disabled.");
-            Assert.AreEqual("It's disabled!", page.GetInputMessage());
         }
     }
 }
