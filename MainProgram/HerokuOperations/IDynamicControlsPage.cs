45de7f79f3146173d7126f947dbdfca25431108c
s3hwag <sehwagvijay31@gmail.com>
2025-08-01 12:16:10 +0530

    Documentation using proper convention for Interfaces

--- MainProgram/HerokuOperations/IDynamicControlsPage.cs
+++ MainProgram/HerokuOperations/IDynamicControlsPage.cs
@@ -1,16 +1,59 @@
-﻿namespace HerokuOperations
+﻿/*
+* Copyright © 2025 Sehwag Vijay
+* All rights reserved.
+*/
+
+namespace HerokuOperations
 {
+    /// <summary>
+    /// Interface defining operations for interacting with the Dynamic Controls page.
+    /// This includes operations for handling checkbox visibility and input field enabling/disabling.
+    /// </summary>
     public interface IDynamicControlsPage
     {
-        // Checkbox section
+        // -------- Checkbox Section --------
+
+        /// <summary>
+        /// Clicks the button to remove or add the checkbox dynamically.
+        /// </summary>
         void ClickRemoveOrAddButton();
+
+        /// <summary>
+        /// Determines whether the checkbox is currently displayed on the page.
+        /// </summary>
+        /// <returns>True if the checkbox is visible; otherwise, false.</returns>
         bool IsCheckboxDisplayed();
+
+        /// <summary>
+        /// Gets the message shown after the checkbox is added or removed.
+        /// </summary>
+        /// <returns>The feedback message related to the checkbox action.</returns>
         string GetCheckboxMessage();
 
-        // Input field section
+
+        // -------- Input Field Section --------
+
+        /// <summary>
+        /// Clicks the button to enable or disable the input field dynamically.
+        /// </summary>
         void ClickEnableOrDisableButton();
+
+        /// <summary>
+        /// Checks whether the input field is currently enabled.
+        /// </summary>
+        /// <returns>True if the input field is enabled; otherwise, false.</returns>
         bool IsInputEnabled();
+
+        /// <summary>
+        /// Enters the specified text into the input field.
+        /// </summary>
+        /// <param name="text">Text to be entered into the input field.</param>
         void EnterText(string text);
+
+        /// <summary>
+        /// Gets the message shown after the input field is enabled or disabled.
+        /// </summary>
+        /// <returns>The feedback message related to the input field action.</returns>
         string GetInputMessage();
     }
 }
--- MainProgram/HerokuOperations/IDynamicLoading.cs
+++ MainProgram/HerokuOperations/IDynamicLoading.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+* Copyright © 2025 Sehwag Vijay
+* All rights reserved.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
