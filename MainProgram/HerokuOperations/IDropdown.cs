f7284ca8c6cc0469b1091935124ac679de24c0cf
Keyur Nagvekar <106338391+Keyurnagvekar@users.noreply.github.com>
2025-08-01 13:33:12 +0530

    Update IDropdown.cs

--- MainProgram/HerokuOperations/IDropdown.cs
+++ MainProgram/HerokuOperations/IDropdown.cs
@@ -1,37 +1,11 @@
-using OpenQA.Selenium;
-
 namespace HerokuOperations
 {
-    public class ShadowDOMPage : IShadowDOM
+    public interface IDropdownPage
     {
-        private readonly IWebDriver _driver;
+        string GetTitle();
 
-        public ShadowDOMPage(IWebDriver driver)
-        {
-            _driver = driver;
-        }
-
-        public string GetFirstShadowHostText()
-        {
-            var shadowHost = _driver.FindElement(By.CssSelector("css-selector-for-first-shadow-host"));
-            var shadowRoot = (IWebElement)((IJavaScriptExecutor)_driver).ExecuteScript("return arguments[0].shadowRoot", shadowHost);
-            return shadowRoot.FindElement(By.CssSelector("css-selector-for-element")).Text;
-        }
-
-        public string GetSecondShadowHostText()
-        {
-            var shadowHost = _driver.FindElement(By.CssSelector("css-selector-for-second-shadow-host"));
-            var shadowRoot = (IWebElement)((IJavaScriptExecutor)_driver).ExecuteScript("return arguments[0].shadowRoot", shadowHost);
-            return shadowRoot.FindElement(By.CssSelector("css-selector-for-element")).Text;
-        }
-
-        public string GetNestedShadowText()
-        {
-            var shadowHost = _driver.FindElement(By.CssSelector("css-selector-for-nested-shadow-host"));
-            var shadowRoot = (IWebElement)((IJavaScriptExecutor)_driver).ExecuteScript("return arguments[0].shadowRoot", shadowHost);
-            var nestedShadowHost = shadowRoot.FindElement(By.CssSelector("css-selector-for-nested-shadow-host"));
-            var nestedShadowRoot = (IWebElement)((IJavaScriptExecutor)_driver).ExecuteScript("return arguments[0].shadowRoot", nestedShadowHost);
-            return nestedShadowRoot.FindElement(By.CssSelector("css-selector-for-element")).Text;
-        }
+        string[] GetAllOptions();
+        void SelectOptionByText(string optionText);
+        string GetSelectedOption();
     }
 }
