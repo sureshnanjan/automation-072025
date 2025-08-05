2b22317a8c3093b4d3d4b7f3f502f83f5346aaf7
Keyurnagvekar <keyurnagvekar03@gmail.com>
2025-08-05 16:53:52 +0530

    herokuappmobile

---
+++ MainProgram/HerokuAppMobile/HomePage.cs
@@ -0,0 +1,31 @@
+﻿using HerokuOperations;
+namespace HerokuAppMobile
+{
+    public class HomePage : IHomePage
+    {
+        public string[] getAvailableExamples()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRepoUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSubTitle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTitle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GoToExample(string exampleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
