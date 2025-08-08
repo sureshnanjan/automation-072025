58e14d0db0d587af428086b7e16ba0713e8d892b
Elangovan R <123308498+Elango-17@users.noreply.github.com>
2025-08-09 00:20:19 +0530

    Commiting shopping_website_db activity with schema and sample data

--- database/sp_check.sql
+++ database/sp_check.sql
@@ -5,10 +5,11 @@
 -- sp_check.sql
 -- Switch to the shopping_website_db database
 USE shopping_website_db;
-
+  
 -- Test the stored procedure 'GetCustomerOrders' for customer with ID = 1
 CALL GetCustomerOrders(1);
 
 -- Test the stored function 'TotalOrderValue' for customer with ID = 1
 -- Returns the total value of orders placed by that customer
 SELECT TotalOrderValue(1) AS order_value;
+
