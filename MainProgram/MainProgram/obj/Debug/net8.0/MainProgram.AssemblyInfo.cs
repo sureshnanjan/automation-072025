519b1c1b8f938b52ffaf55e53a1c78b09f4eaa59
Shreya-ascd <shreya0203.ascd@gmail.com>
2025-08-23 15:45:09 +0530

    feat(test): add Petstore JMX test plan recorded via BlazeMeter for performance testing

--- MainProgram/MainProgram/obj/Debug/net8.0/MainProgram.AssemblyInfo.cs
+++ MainProgram/MainProgram/obj/Debug/net8.0/MainProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e47db4407c8ca3330997e0480ee37793e1c881b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+421177565fd0a406a23af703d31c9c4bea5a2543")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
